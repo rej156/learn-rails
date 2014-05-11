666b9996b75bd74c58243c26fd72a6924a41c6d6
Eric John Juta <ericjohnjuta@yahoo.co.uk>
2014-05-11 02:47:28 +0100

    Added Rails.logger.debug STRING statements to controller and model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -9,6 +9,7 @@ class Owner
     end
 
     def countdown
+        Rails.logger.debug "DEBUG: Entering Owner countdown method"
         today = Date.today
         birthday = Date.new(today.year, birthdate.month, birthdate.day)
         if birthday > today
